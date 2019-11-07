5b4ef1a67fc4c91433ad9044dc667eadbbf8ad16
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-11-07 20:21:33 +0000

    First commit

---
+++ app/Http/Controllers/NameController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Name;
+use Illuminate\Http\Request;
+
+class NameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $names = Name::all();
+
+        return view('welcome', compact('names'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //dd($request);
+        $userId = $request->user_id;
+        $user   =   User::updateOrCreate(['id' => $userId],
+                    ['name' => $request->name]);
+    
+        return Response::json($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Name  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Name $name)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Name  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Name $name)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Name  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Name $name)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Name  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Name $name)
+    {
+        //
+    }
+}
