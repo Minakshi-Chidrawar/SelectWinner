c7bad0fdba15a4b54e5fe9e3c203197906f29466
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-11-13 20:47:51 +0000

    Added reoute to show random names

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', 'NameController@index');
 Route::get('create', 'NameController@create');
 Route::post('/', 'NameController@store')->name('store');
 Route::delete('/{id}', 'NameController@destroy')->name('delete');
+Route::get('/{numberOfWinners}', 'SelectWinnersController@show')->name('show');
