78961e93b8e1c6d4df5ba798e9a915f603ac7414
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-11-14 20:54:50 +0000

    Added action to the form

--- resources/views/selectWinner/numberToSelectWinner.blade.php
+++ resources/views/selectWinner/numberToSelectWinner.blade.php
@@ -1,27 +1,28 @@
-@csrf
-<div class="modal fade" id="selectWinnerModal" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="titleModal"></h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form id="selectWinnerForm" name="selectWinnerForm" class="col-form-label">
-                    <div class="form-group">
-                        <label for="numberOfWinners" class="control-label">Enter the numbers to select winners</label>
-                        <input type="number" class="form-control" id="numberOfWinners" name="numberOfWinners" placeholder="Enter Number" min="1" value="" required="">
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <!-- a href="{{ route('show')}}"> -->
+
+<form action="/show" method="GET">
+    @csrf
+    <div class="modal fade" id="selectWinnerModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="titleModal"></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="selectWinnerForm" name="selectWinnerForm" class="col-form-label">
+                        <div class="form-group">
+                            <label for="numberOfWinners" class="control-label">Enter the numbers to select winners</label>
+                            <input type="number" class="form-control" id="numberOfWinners" name="numberOfWinners" placeholder="Enter Number" min="1" value="" required="">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
                     <button type="submit" class="btn btn-primary" id="numberToSelect" value="create">Select Winner</button>
-                <!-- /a> -->
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</form>
