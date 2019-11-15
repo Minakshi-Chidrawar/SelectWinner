f20ea4558d34442d5bd930df62c80b6e0fb43ff5
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-11-15 22:07:55 +0000

    Tidy up

--- app/Http/Controllers/SelectWinnersController.php
+++ app/Http/Controllers/SelectWinnersController.php
@@ -8,21 +8,16 @@ use Illuminate\Http\Request;
 
 class SelectWinnersController extends Controller
 {
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Name  $name
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Request $request)
+    public function winners(Request $request)
     {
-        //dd($request->numberOfWinners);
         $names = Name::all();
         $winnerNames = Name::inRandomOrder()->take($request->numberOfWinners)->get();
-
-        //dd($names);
-        //return Response::json($winnerNames);
+        
         return view('selectWinner.index', compact('names', 'winnerNames'));
-        //return redirect::route('index', ['names' => $names, 'winnerNames' => $winnerNames]);
+    }
+
+    public function clear()
+    {
+        return redirect()->action('NameController@index');
     }
 }
