a96400096bea1d4261bdaa4edbcdb1d0d1778951
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-11-15 22:11:24 +0000

    Added button to go back to the index page

--- resources/views/selectWinner/index.blade.php
+++ resources/views/selectWinner/index.blade.php
@@ -5,39 +5,55 @@
 @section('content')
     <div class="container py-4">
         <div class="row mb-3">
-            <div class="col-6">
-                <button type="submit" class="btn btn-success mb-2 addName" id="addName">Add User</button>
-            </div>
-            <div class="col-6">
-                @if(count($names) > 2)
+            <div class="col-12">
+                <button type="submit" class="btn btn-success mb-2 addName" id="add">Add User</button>               
+
+                @if(count($names ?? '') > 2)
                     <button type="submit" class="btn btn-success mb-2 selectWinner" id="selectWinner">Select Winner</button>
+                @endif
+
+                @if(count($winnerNames ?? '') > 0)
+                    <button type="submit" class="btn btn-success mb-2 selectWinner" id="clearWinners">
+                        <a href="clear">Reset winners list</a>
+                    </button>
                 @endif
             </div>
         </div>
         <div class="row">
             <div class="col-4">
-                <table class="table table-borderless table-hover" id="nameList">
-                    <tbody id="users-crud">
-                        @foreach($names as $name)
-                        <tr id="user_id_{{ $name->id }}">
-                            <td>{{ $name->name }}</td>
-                            <td>
-                                <a href="javascript:void(0)" id="editName" data-id="{{ $name->id }}" data-name="{{$name->name}}" class="btn btn-info">
-                                    <span class="fa fa-pencil"></span>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="javascript:void(0)" id="deleteName" data-id="{{ $name->id }}" data-name="{{$name->name}}" class="btn btn-danger delete-user">
-                                    <span class="fa fa-minus"></span>
-                                </a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                @if(count($names) > 0)
+                    <h2 class="user-list">List of users</h2>
+                    <table class="table table-borderless table-hover" id="nameList">
+                        <tbody id="users-crud">
+                            @foreach($names ?? '' as $name)
+                            <tr id="user_id_{{ $name->id }}">
+                                <td>{{ $name->name }}</td>
+                                <td>
+                                    <a href="javascript:void(0)" id="editName" data-id="{{ $name->id }}" data-name="{{$name->name}}" class="btn btn-info btn-sm">
+                                        <span class="fa fa-pencil"></span>
+                                    </a>
+                                    
+                                    <a href="javascript:void(0)" id="deleteName" data-id="{{ $name->id }}" data-name="{{$name->name}}" class="btn btn-danger btn-sm delete-user">
+                                        <span class="fa fa-minus"></span>
+                                    </a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @endif
             </div>
-            <div class="col-6 col-offset-2">
-                <div class="winnerList"></div>
+            <div class="col-6 offset-1">
+                @if(count($winnerNames) > 0)
+                    <div class="winnerList shadow p-3 mb-5 bg-white rounded">
+                        <h2 class="winner-heading">Winners are</h2>
+                        <table class="table table-borderless table-hover">
+                            @foreach($winnerNames as $winnerName)
+                                <tr><td><strong>{{ $winnerName->name }}</strong></td></tr>
+                            @endforeach
+                        </table>
+                    </div>
+                @endif
             </div> 
         </div>
     </div>
