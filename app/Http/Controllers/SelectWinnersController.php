034116a942694167860a2ebf5d65779d39e2dfc8
Minakshi Chidrawar <MChidrawar@britishmuseum.org>
2019-11-21 20:39:48 +0000

    Added check for the number entered to select the winners should be less than the existing users

--- app/Http/Controllers/SelectWinnersController.php
+++ app/Http/Controllers/SelectWinnersController.php
@@ -10,8 +10,19 @@ class SelectWinnersController extends Controller
 {
     public function winners(Request $request)
     {
+        $numberToSelectWinner = $request->numberOfWinners;
         $names = Name::all();
-        $winnerNames = Name::inRandomOrder()->take($request->numberOfWinners)->get();
+
+        if (count($names) < $numberToSelectWinner)
+        {
+            $errorMsg = 'You have entered the number ' . $numberToSelectWinner . ' to select the winners.'
+                      . ' But there are only ' . count($names) . ' users exists.' 
+                      . ' Please enter the number less than ' . count($names) . '.';
+
+            return redirect()->action('NameController@index')->withFail($errorMsg); 
+        }
+
+        $winnerNames = Name::inRandomOrder()->take($numberToSelectWinner)->get();
         
         return view('selectWinner.index', compact('names', 'winnerNames'));
     }
